af26d79e90891adebe23cd388d2df8b5fcf63afc
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-07-08 15:59:15 -0700

    React-Router-DOM Set Up + Added an asset image

--- freely/src/App.js
+++ freely/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Route from "react-router-dom";
 
 /* Component Imports */
 import Navigation from "./component/navigation/index.js";
@@ -11,8 +12,19 @@ function App() {
         <Navigation />
       </header>
       <main>
-        Hero content
+        {/* <Route exact path = "/" component = {} />
+        <Route path = "/login" component = {} />
+        <Route path = "/relax" component = {} /> */}
       </main>
+
+      {/* <footer>
+        Icons made by
+        <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
+        from
+        <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a>
+        is licensed by
+        <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+      </footer> */}
     </div>
   );
 }
