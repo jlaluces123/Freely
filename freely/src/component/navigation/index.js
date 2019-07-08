aa817427d65d9b6b92c536c47580612edebd44f0
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-07-08 15:49:00 -0700

    File Set Up + Navigation Work: Styled up the navigation a little as well as set up the file structure for the other views

--- freely/src/component/navigation/index.js
+++ freely/src/component/navigation/index.js
@@ -4,18 +4,18 @@ import "./navigation.css";
 const Navigation = () => {
   return (
     <div className = "navigation__container">
-      <div className="navigation__logo">
-        {/* Logo Here */}
+      {/* Left Side of nav */}
+      <div className = "navigation__logo">
+        <h3 className = "navigation__header">Freely</h3>
       </div>
 
-      <div className="navigation">
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Relax</li>
-            <li>About Us</li>
-          </ul>
-        </nav>
+      {/* Right Side of nav */}
+      <div className = "navigation">
+        <ul className = "navigation__list">
+          <li className = "navigation__item">Home</li>
+          <li className = "navigation__item">Relax</li>
+          <li className = "navigation__item">About Us</li>
+        </ul>
       </div>
 
     </div>
