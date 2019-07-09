318606d213c325689749174cc4e485848730f263
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-07-09 13:42:57 -0700

    Home Component: Following the design of BrainFM website, set up the main home component so far with filler content, needs styling

--- freely/src/component/home/index.js
+++ freely/src/component/home/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import "./home.css";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="home__left">
+    <div className = "home">
+      <div className = "home__left">
         {/* Content Here */}
+        <h3 className = "home__header">Lorem ipsum dolor sit amet.</h3>
+        <h2 className = "home__header--bold">Lorem ipsum dolor sit amet consectetur.</h2>
+        <p className = "home__paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, molestiae recusandae fuga sunt a consectetur sequi facilis impedit doloremque vitae.</p>
       </div>
 
-      <div className="home__right">
+      <div className = "home__right">
         {/* Image Here */}
+        <img src="../assets/meditation.png" alt="Icon for a person sitting criss cross"/>
       </div>
     </div>
   );
